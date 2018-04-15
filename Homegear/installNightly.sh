902441f1982636122b7d1fda2307837f83cd989f
Christian Lahme <christian.lahme@googlemail.com>
2018-04-15 08:37:34 +0200

    Make downgrade possible

--- Homegear/installNightly.sh
+++ Homegear/installNightly.sh
@@ -10,6 +10,9 @@ apt-get update
 system="debian_stretch"
 arch="amd64"
 
+# put date or current
+cversion="20180413"
+
 function downloadModule {
 	wget https://homegear.eu/downloads/nightlies/${1} || exit 1
 }
@@ -27,110 +30,110 @@ cd $TEMPDIR
 rm -f homegear*.deb
 rm -f libhomegear*.deb
 
-wget https://homegear.eu/downloads/nightlies/libhomegear-base_current_${system}_${arch}.deb || exit 1
-wget https://homegear.eu/downloads/nightlies/libhomegear-node_current_${system}_${arch}.deb || exit 1
-wget https://homegear.eu/downloads/nightlies/libhomegear-ipc_current_${system}_${arch}.deb || exit 1
-wget https://homegear.eu/downloads/nightlies/homegear_current_${system}_${arch}.deb || exit 1
-wget https://homegear.eu/downloads/nightlies/homegear-nodes-core_current_${system}_${arch}.deb || exit 1
-wget https://homegear.eu/downloads/nightlies/homegear-nodes-extra_current_${system}_${arch}.deb || exit 1
-wget https://homegear.eu/downloads/nightlies/homegear-licensing_current_${system}_${arch}.deb || exit 1
-wget https://homegear.eu/downloads/nightlies/homegear-easy-licensing_current_${system}_${arch}.deb || exit 1
+wget https://homegear.eu/downloads/nightlies/libhomegear-base_${cversion}_${system}_${arch}.deb || exit 1
+wget https://homegear.eu/downloads/nightlies/libhomegear-node_${cversion}_${system}_${arch}.deb || exit 1
+wget https://homegear.eu/downloads/nightlies/libhomegear-ipc_${cversion}_${system}_${arch}.deb || exit 1
+wget https://homegear.eu/downloads/nightlies/homegear_${cversion}_${system}_${arch}.deb || exit 1
+wget https://homegear.eu/downloads/nightlies/homegear-nodes-core_${cversion}_${system}_${arch}.deb || exit 1
+wget https://homegear.eu/downloads/nightlies/homegear-nodes-extra_${cversion}_${system}_${arch}.deb || exit 1
+wget https://homegear.eu/downloads/nightlies/homegear-licensing_${cversion}_${system}_${arch}.deb || exit 1
+wget https://homegear.eu/downloads/nightlies/homegear-easy-licensing_${cversion}_${system}_${arch}.deb || exit 1
 
-downloadModule homegear-homematicbidcos_current_${system}_${arch}.deb
-#downloadModule homegear-homematicwired_current_${system}_${arch}.deb
-#downloadModule homegear-insteon_current_${system}_${arch}.deb
-#downloadModule homegear-max_current_${system}_${arch}.deb
-#downloadModule homegear-philipshue_current_${system}_${arch}.deb
-#downloadModule homegear-sonos_current_${system}_${arch}.deb
-#downloadModule homegear-kodi_current_${system}_${arch}.deb
-#downloadModule homegear-ipcam_current_${system}_${arch}.deb
-#downloadModule homegear-beckhoff_current_${system}_${arch}.deb
-#downloadModule homegear-knx_current_${system}_${arch}.deb
-#downloadModule homegear-enocean_current_${system}_${arch}.deb
-#downloadModule homegear-intertechno_current_${system}_${arch}.deb
-#downloadModule homegear-rs2w_current_${system}_${arch}.deb
-#downloadModule homegear-rsl_current_${system}_${arch}.deb
-#downloadModule homegear-zwave_current_${system}_${arch}.deb
-#downloadModule homegear-mbus_current_${system}_${arch}.deb
-#downloadModule homegear-ccu2_current_${system}_${arch}.deb
-#downloadModule homegear-easycam_current_${system}_${arch}.deb
-#downloadModule homegear-easyled_current_${system}_${arch}.deb
-#downloadModule homegear-easyled2_current_${system}_${arch}.deb
-#downloadModule homegear-influxdb_current_${system}_${arch}.deb
+downloadModule homegear-homematicbidcos_${cversion}_${system}_${arch}.deb
+#downloadModule homegear-homematicwired_${cversion}_${system}_${arch}.deb
+#downloadModule homegear-insteon_${cversion}_${system}_${arch}.deb
+#downloadModule homegear-max_${cversion}_${system}_${arch}.deb
+#downloadModule homegear-philipshue_${cversion}_${system}_${arch}.deb
+#downloadModule homegear-sonos_${cversion}_${system}_${arch}.deb
+#downloadModule homegear-kodi_${cversion}_${system}_${arch}.deb
+#downloadModule homegear-ipcam_${cversion}_${system}_${arch}.deb
+#downloadModule homegear-beckhoff_${cversion}_${system}_${arch}.deb
+#downloadModule homegear-knx_${cversion}_${system}_${arch}.deb
+#downloadModule homegear-enocean_${cversion}_${system}_${arch}.deb
+#downloadModule homegear-intertechno_${cversion}_${system}_${arch}.deb
+#downloadModule homegear-rs2w_${cversion}_${system}_${arch}.deb
+#downloadModule homegear-rsl_${cversion}_${system}_${arch}.deb
+#downloadModule homegear-zwave_${cversion}_${system}_${arch}.deb
+#downloadModule homegear-mbus_${cversion}_${system}_${arch}.deb
+#downloadModule homegear-ccu2_${cversion}_${system}_${arch}.deb
+#downloadModule homegear-easycam_${cversion}_${system}_${arch}.deb
+#downloadModule homegear-easyled_${cversion}_${system}_${arch}.deb
+#downloadModule homegear-easyled2_${cversion}_${system}_${arch}.deb
+#downloadModule homegear-influxdb_${cversion}_${system}_${arch}.deb
 
-dpkg -i libhomegear-base_current_${system}_${arch}.deb
+dpkg -i libhomegear-base_${cversion}_${system}_${arch}.deb
 if [ $? -ne 0 ]; then
 	apt-get -y -f install || exit 1
-	dpkg -i libhomegear-base_current_${system}_${arch}.deb || exit 1
+	dpkg -i libhomegear-base_${cversion}_${system}_${arch}.deb || exit 1
 fi
 
-dpkg -i libhomegear-node_current_${system}_${arch}.deb
+dpkg -i libhomegear-node_${cversion}_${system}_${arch}.deb
 if [ $? -ne 0 ]; then
 	apt-get -y -f install || exit 1
-	dpkg -i libhomegear-node_current_${system}_${arch}.deb || exit 1
+	dpkg -i libhomegear-node_${cversion}_${system}_${arch}.deb || exit 1
 fi
 
-dpkg -i libhomegear-ipc_current_${system}_${arch}.deb
+dpkg -i libhomegear-ipc_${cversion}_${system}_${arch}.deb
 if [ $? -ne 0 ]; then
 	apt-get -y -f install || exit 1
-	dpkg -i libhomegear-ipc_current_${system}_${arch}.deb || exit 1
+	dpkg -i libhomegear-ipc_${cversion}_${system}_${arch}.deb || exit 1
 fi
 
-dpkg -i homegear_current_${system}_${arch}.deb
+dpkg -i homegear_${cversion}_${system}_${arch}.deb
 if [ $? -ne 0 ]; then
 	apt-get -y -f install || exit 1
-	dpkg -i homegear_current_${system}_${arch}.deb
+	dpkg -i homegear_${cversion}_${system}_${arch}.deb
 	if [ $? -ne 0 ]; then
 		apt-get -y -f install || exit 1
-		dpkg -i homegear_current_${system}_${arch}.deb || exit 1
+		dpkg -i homegear_${cversion}_${system}_${arch}.deb || exit 1
 	fi
 fi
 
-dpkg -i homegear-nodes-core_current_${system}_${arch}.deb
+dpkg -i homegear-nodes-core_${cversion}_${system}_${arch}.deb
 if [ $? -ne 0 ]; then
 	apt-get -y -f install || exit 1
-	dpkg -i homegear-nodes-core_current_${system}_${arch}.deb || exit 1
+	dpkg -i homegear-nodes-core_${cversion}_${system}_${arch}.deb || exit 1
 fi
 
-dpkg -i homegear-nodes-extra_current_${system}_${arch}.deb
+dpkg -i homegear-nodes-extra_${cversion}_${system}_${arch}.deb
 if [ $? -ne 0 ]; then
 	apt-get -y -f install || exit 1
-	dpkg -i homegear-nodes-extra_current_${system}_${arch}.deb || exit 1
+	dpkg -i homegear-nodes-extra_${cversion}_${system}_${arch}.deb || exit 1
 fi
 
-dpkg -i homegear-licensing_current_${system}_${arch}.deb
+dpkg -i homegear-licensing_${cversion}_${system}_${arch}.deb
 if [ $? -ne 0 ]; then
 	apt-get -y -f install || exit 1
-	dpkg -i homegear-licensing_current_${system}_${arch}.deb || exit 1
+	dpkg -i homegear-licensing_${cversion}_${system}_${arch}.deb || exit 1
 fi
 
-dpkg -i homegear-easy-licensing_current_${system}_${arch}.deb
+dpkg -i homegear-easy-licensing_${cversion}_${system}_${arch}.deb
 if [ $? -ne 0 ]; then
 	apt-get -y -f install || exit 1
-	dpkg -i homegear-easy-licensing_current_${system}_${arch}.deb || exit 1
+	dpkg -i homegear-easy-licensing_${cversion}_${system}_${arch}.deb || exit 1
 fi
 
-installModule homegear-homematicbidcos_current_${system}_${arch}.deb
-#installModule homegear-homematicwired_current_${system}_${arch}.deb
-#installModule homegear-insteon_current_${system}_${arch}.deb
-#installModule homegear-max_current_${system}_${arch}.deb
-#installModule homegear-philipshue_current_${system}_${arch}.deb
-#installModule homegear-sonos_current_${system}_${arch}.deb
-#installModule homegear-kodi_current_${system}_${arch}.deb
-#installModule homegear-ipcam_current_${system}_${arch}.deb
-#installModule homegear-beckhoff_current_${system}_${arch}.deb
-#installModule homegear-knx_current_${system}_${arch}.deb
-#installModule homegear-enocean_current_${system}_${arch}.deb
-#installModule homegear-intertechno_current_${system}_${arch}.deb
-#installModule homegear-rs2w_current_${system}_${arch}.deb
-#installModule homegear-rsl_current_${system}_${arch}.deb
-#installModule homegear-zwave_current_${system}_${arch}.deb
-#installModule homegear-mbus_current_${system}_${arch}.deb
-#installModule homegear-ccu2_current_${system}_${arch}.deb
-#installModule homegear-easycam_current_${system}_${arch}.deb
-#installModule homegear-easyled_current_${system}_${arch}.deb
-#installModule homegear-easyled2_current_${system}_${arch}.deb
-#installModule homegear-influxdb_current_${system}_${arch}.deb
+installModule homegear-homematicbidcos_${cversion}_${system}_${arch}.deb
+#installModule homegear-homematicwired_${cversion}t_${system}_${arch}.deb
+#installModule homegear-insteon_${cversion}_${system}_${arch}.deb
+#installModule homegear-max_${cversion}_${system}_${arch}.deb
+#installModule homegear-philipshue_${cversion}_${system}_${arch}.deb
+#installModule homegear-sonos_${cversion}_${system}_${arch}.deb
+#installModule homegear-kodi_${cversion}_${system}_${arch}.deb
+#installModule homegear-ipcam_${cversion}_${system}_${arch}.deb
+#installModule homegear-beckhoff_${cversion}_${system}_${arch}.deb
+#installModule homegear-knx_${cversion}_${system}_${arch}.deb
+#installModule homegear-enocean_${cversion}_${system}_${arch}.deb
+#installModule homegear-intertechno_${cversion}_${system}_${arch}.deb
+#installModule homegear-rs2w_${cversion}_${system}_${arch}.deb
+#installModule homegear-rsl_${cversion}_${system}_${arch}.deb
+#installModule homegear-zwave_${cversion}_${system}_${arch}.deb
+#installModule homegear-mbus_${cversion}_${system}_${arch}.deb
+#installModule homegear-ccu2_${cversion}_${system}_${arch}.deb
+#installModule homegear-easycam_${cversion}_${system}_${arch}.deb
+#installModule homegear-easyled_${cversion}_${system}_${arch}.deb
+#installModule homegear-easyled2_${cversion}_${system}_${arch}.deb
+#installModule homegear-influxdb_${cversion}_${system}_${arch}.deb
 
 rm -f /etc/homegear/dh1024.pem
 rm -f /etc/homegear/homegear.key
