d1aade9f2da275479b2633166f7802cf0e29e327
Christian Lahme <christian.lahme@googlemail.com>
2018-10-06 23:19:47 +0200

    Update installNightly.sh

--- Homegear/installNightly.sh
+++ Homegear/installNightly.sh
@@ -11,7 +11,7 @@ system="debian_stretch"
 arch="amd64"
 
 # put date or current
-cversion="20180922"
+cversion="20181004"
 
 function downloadModule {
 	wget https://homegear.eu/downloads/nightlies/${1} || exit 1
